f863a94882d6d525f3711c13f61cbd508c4fe376
dauzer <rybachuk.iaroslav@gmail.com>
2019-10-14 10:06:39 +0300

    change

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <?php
 session_start();
-const ROOT = __DIR__.'/files';
+const ROOT = __DIR__ . '/files';
 const INDEX = '/files';
 require_once 'helper.php';
-if (!empty($_POST['create'])){
+
+$folders = [];
+if (!empty($_POST['create'])) {
     $dir = ROOT;
     createFolder($dir, $_POST['create']);
-}
-$files = scandir($dir);
-foreach ($files as $file){
-    if ($file != '.' && $file != '..' & $file != '.DS_Store') {
-        $currentDir[] = $file;
-    }
+} else {
+    $dir = ROOT;
 }
 
-if (!empty($_POST['delete'])) {
-    $folderName = $_POST['delete'];
+if (!empty($_POST['delete-folder'])) {
+    var_dump($_POST);
+    $folderName = $_POST['delete-folder'];
+//    var_dump($dir);
     try {
         rmdir($dir . DIRECTORY_SEPARATOR . $folderName);
     } catch (Error $e) {
 
     }
 }
+
+$files = scandir($dir);
+foreach ($files as $file) {
+    if ($file != '.' && $file != '..' & $file != '.DS_Store') {
+        $currentDir[] = $file;
+        array_push($folders, $file);
+    }
+}
+
+
 
 p($dir . DIRECTORY_SEPARATOR . $folderName);
 ?>
@@ -30,12 +40,13 @@ p($dir . DIRECTORY_SEPARATOR . $folderName);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-<form action="" method="post" >
+<form action="" method="post">
     <input type="text" name="create">
     <input type="submit" value="create to folder">
 </form>
@@ -43,14 +54,19 @@ p($dir . DIRECTORY_SEPARATOR . $folderName);
 <h1>Files:</h1>
 
 <?php
-foreach ($currentDir as $value) {
-    echo "$value
-<form method=\"post\" action=\"#\">
-   <input type='text' name='delete' value='$value'>
-        <input value='delete $value' type='submit'>
-    </form>";
+var_dump($folders);
+if (!empty($folders)) {
+    foreach ($folders as $folderName) {
+        ?>
+        <form method="post" action="#">
+            <?= $folderName ?>
+            <input type='text' name='delete-folder' value='<?= $folderName ?>' hidden>
+            <input value='delete <?= $folderName ?>' type='submit'>
+        </form>
+        <?php
+    }
 }
-       ?>
+//?>
 
 </body>
 </html>
